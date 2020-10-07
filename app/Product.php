73cbc1908572a92078ff7aaaea85de8cc58e52c5
cuchuoiddd <tiennm298@gmail.com>
2020-10-07 18:08:48 +0700

    fix

--- app/Product.php
+++ app/Product.php
@@ -8,20 +8,25 @@ class Product extends Model
 {
     protected $guarded = [];
 
-    public function category(){
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
 
-    public function people(){
+    public function people()
+    {
         return $this->belongsTo(People::class);
     }
 
-    public function getPeopleVN(){
-        $people = People::whereIn('id',json_decode($this->design_team))->pluck('full_name_vn')->toArray();
-        return implode(', ',$people);
+    public function getPeopleVN()
+    {
+        $people = People::whereIn('id', json_decode($this->design_team))->pluck('full_name_vn')->toArray();
+        return count($people) ? implode(', ', $people) : '';
     }
-    public function getPeopleEN(){
-        $people = People::whereIn('id',json_decode($this->design_team))->pluck('full_name_en')->toArray();
-        return implode(', ',$people);
+
+    public function getPeopleEN()
+    {
+        $people = People::whereIn('id', json_decode($this->design_team))->pluck('full_name_en')->toArray();
+        return count($people) ? implode(', ', $people) : '';
     }
 }
